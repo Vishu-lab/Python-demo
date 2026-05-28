08e10d9b8442b84fda5471fe067ac9c1911fee8d
Vidhi-Bansal <vidhibansal528@gmail.com>
2026-05-28 16:13:10 +0530

    Adding Basic instructions

---
+++ datatype.py
@@ -0,0 +1,29 @@
+#Numbers
+x = -5
+print(x)
+print(type(x))
+#String
+a = "Hello world"
+print(a)
+print (type(a))
+#float 
+b = 2.35
+print(b)
+print(type(b))
+c= "dog said woof"
+print(c)
+print(type(c))
+# Using escape characters like \n,\t,\r
+d= "this is a line \n along with a new line"
+print(d)
+print(type(d)) 
+e="this is a line \t along with a new line"
+print(e)
+f="this is a line \n              \ralong with a new line"
+print(f)
+g="this is a line \n                 along with a new line"
+print(g)
+# Boolean
+h= True
+print(h)
+print(type(h))
---
+++ first-program.py
@@ -0,0 +1,3 @@
+# Hey, this is my first program
+""" Today I am learning to add comments and using of print statement"""
+print ("Hello Python")
