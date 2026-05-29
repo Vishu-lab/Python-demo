ac325ce898a4ca2febb4d578c79ef7855034cd20
Vidhi-Bansal <vidhibansal528@gmail.com>
2026-05-29 23:23:43 +0530

    Adding operators basic

---
+++ operators.py
@@ -0,0 +1,34 @@
+# Using Arithmetic Operators
+a=3
+b=1
+#Subtraction
+print(a-b)
+print(type(a-b))
+#Addition
+print(a+b)
+print(type(a+b))
+print(1+1)
+#Multiplication
+print(a*b)
+print(type(a*b))
+#Division
+c=a/b
+print(c)
+print(type(c))
+#Modulous
+print(a%b)
+print(type(a%b))
+#Extra
+a= 1*2
+b= 5*6
+print(a*b)
+
+
+#Comparison Operator
+a=5
+b=9
+print(a<b)
+print(a>b)
+print(a!=b)
+
+#Boolean Operators
